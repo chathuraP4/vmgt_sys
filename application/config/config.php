f04a4c02654b4424f97ce323e670a094c0ad2aba
chathuraP4 <chathurangaliyanage2018@gmail.com>
2022-07-01 18:38:48 +0530

    setup

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/vsmsatp3/';
+$config['base_url'] = 'http://localhost/vmgt_sys/';
 
 /*
 |--------------------------------------------------------------------------
